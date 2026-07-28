522bf4fea05af36de606adac0661818dc3ac381d
deletrics-oss <deletrics@gmail.com>
2026-07-28 01:01:51 -0300

    Upload Helena Flores system: _DISTRIBUTION_COPY/novas integracoes/db.php

---
+++ _DISTRIBUTION_COPY/novas integracoes/db.php
@@ -0,0 +1,10 @@
+<?php
+
+try {
+    $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro na conexão com o banco de dados: " . $e->getMessage());
+}
+
+?>
