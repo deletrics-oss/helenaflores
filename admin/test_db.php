f26ea8b1c5faa563f97e595b472fad3dc50ccd5a
deletrics-oss <deletrics@gmail.com>
2026-07-28 00:48:02 -0300

    Upload Helena Flores system: admin/test_db.php

---
+++ admin/test_db.php
@@ -0,0 +1,8 @@
+<?php
+require_once __DIR__ . '/../includes/db.php';
+try {
+    \ = \->query("SHOW CREATE TABLE users");
+    print_r(\->fetch());
+} catch(Exception \) {
+    echo \->getMessage();
+}
