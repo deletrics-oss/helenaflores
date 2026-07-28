39bd20a1745f400a7bca4d58588cd22574b18282
deletrics-oss <deletrics@gmail.com>
2026-07-28 00:57:11 -0300

    Upload Helena Flores system: scratch/fetch_instances.php

---
+++ scratch/fetch_instances.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'includes/db.php';
+require_once 'includes/notifications.php';
+$notif = new NotificationService($pdo);
+$cfg = $notif->getConfig();
+$url = rtrim($cfg['notif_waapi_url'] ?? '', '/');
+$key = $cfg['notif_waapi_key'] ?? '';
+
+echo "URL: $url\n";
+echo "KEY: " . substr($key, 0, 5) . "...\n";
+
+$ch = curl_init("$url/instance/fetchInstances");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['apikey: ' . $key]);
+$res = curl_exec($ch);
+echo "INSTANCES:\n$res\n";
