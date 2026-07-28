098f9df4900d19b42a231e9207bce140f346b1b4
deletrics-oss <deletrics@gmail.com>
2026-07-28 00:56:47 -0300

    Upload Helena Flores system: novas integracoes/config.php

---
+++ novas integracoes/config.php
@@ -0,0 +1,8 @@
+<?php
+define("BASE_URL", "http://localhost:8000"); // Altere para a URL do seu site em produção
+
+define("DB_HOST", "localhost");
+define("DB_NAME", "fightarcade");
+define("DB_USER", "root");
+define("DB_PASS", "");
+?>
